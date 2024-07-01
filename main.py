82b02f456473ba8dc5dd589c665c48065a591bba
MarryBye <viktorlukianov@outlook.com>
2024-07-01 12:36:30 +0300

    Added links

--- main.py
+++ main.py
@@ -16,6 +16,30 @@ app.secret_key = "SecretKey"
 
 @app.route("/")
 def index():
-    return render_template("home.html", page_name="Главная")
+    return render_template("index.html", page_name="Главная")
+
+@app.route("/news")
+def news():
+    return render_template("news.html", page_name="Новости")
+
+@app.route("/servers")
+def servers():
+    return render_template("servers.html", page_name="Сервера")
+
+@app.route("/forum")
+def forum():
+    return render_template("forum.html", page_name="Форум")
+
+@app.route("/contacts")
+def contacts():
+    return render_template("contacts.html", page_name="Контакты")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", page_name="Вход")
+
+@app.route("/register")
+def register():
+    return render_template("register.html", page_name="Регистрация")
 
 app.run()
